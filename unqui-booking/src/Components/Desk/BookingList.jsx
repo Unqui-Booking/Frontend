8cc5ac80bbfe7c21b289d3538d903995e38ac855
ericagerez <ericagerez63@gmail.com>
2021-04-16 02:54:07 -0300

    Title resize

--- unqui-booking/src/Components/Desk/BookingList.jsx
+++ unqui-booking/src/Components/Desk/BookingList.jsx
@@ -1,18 +1,18 @@
 import React from 'react';
 import { Grid , Typography, Divider, CardContent} from '@material-ui/core'
 
-const bookings = [1,2,3,4,5,6,]
+const bookings = [1,2,3,4,5,6,7,8,9,10]
 
 const BookingList = () => {
     return (
         <CardContent>
             <Grid container spacing={3}>
                 <Grid item xs={12}>
-                    <Typography variant='h4'> Reservas registradas </Typography>
+                    <Typography variant='h5'> Reservas registradas </Typography>
                     <Divider></Divider>
                 </Grid>
                 {bookings.map(b => 
-                    <Grid item xs={12}>
+                    <Grid item xs={12} key={b}>
                         <p>12-13</p>
                         <Divider></Divider>
                     </Grid>

d87fe116a1442a0c5ef6feee4442159fe53edd29
ericagerez <ericagerez63@gmail.com>
2021-04-16 02:49:20 -0300

    Incorporating navbar and new theme

--- unqui-booking/src/App.js
+++ unqui-booking/src/App.js
@@ -1,8 +1,15 @@
 import BookingHome from "./Components/Desk/BookingHome";
+import { ThemeProvider } from '@material-ui/core/styles'
+import theme from './themeConfig'
+import NavBar from "./Components/Home/NavBar"
 
 function App() {
   return (
-    <BookingHome></BookingHome>
+    <ThemeProvider theme={theme}>
+        <NavBar></NavBar>
+        <BookingHome></BookingHome>
+    </ThemeProvider>
+    
   );
 }
 

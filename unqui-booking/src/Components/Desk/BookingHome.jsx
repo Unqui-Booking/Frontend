d094551c966b3a79316d04b86e4796ede12a190a
ericagerez <ericagerez63@gmail.com>
2021-04-16 03:01:19 -0300

    Adding header to booking home page

--- unqui-booking/src/Components/Desk/BookingHome.jsx
+++ unqui-booking/src/Components/Desk/BookingHome.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Grid, Container , Card } from '@material-ui/core'
 import BookingList from './BookingList'
+import BookingHeader from './BookingHeader'
 import { makeStyles } from '@material-ui/core/styles'
 
 const useStyles = makeStyles((theme) => ({
@@ -19,6 +20,7 @@ const BookingHome = () => {
             <Grid container className={classes.root} justify="center">
             <Grid item xs={12}>
                 <Card>
+                    <BookingHeader></BookingHeader>
                     <BookingList></BookingList>
                 </Card>
             </Grid>

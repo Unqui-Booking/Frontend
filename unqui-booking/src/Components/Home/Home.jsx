ef953a876f2d1f7063f1e354f7ca2dcf6370e4ea
ericagerez <ericagerez63@gmail.com>
2021-04-28 21:08:57 -0300

    Title and picker date for page home

--- unqui-booking/src/Components/Home/Home.jsx
+++ unqui-booking/src/Components/Home/Home.jsx
@@ -1,8 +1,9 @@
 import React from 'react'
-import { Grid, Container } from '@material-ui/core'
+import { Grid, Container,  Typography} from '@material-ui/core'
 import { makeStyles } from '@material-ui/core/styles'
 import DateFnsUtils from '@date-io/date-fns';
-import { MuiPickersUtilsProvider, KeyboardTimePicker, KeyboardDatePicker } from '@material-ui/pickers';
+import { MuiPickersUtilsProvider, KeyboardDatePicker } from '@material-ui/pickers';
+
 
 const useStyles = makeStyles((theme) => ({
     formControl: {
@@ -12,17 +13,29 @@ const useStyles = makeStyles((theme) => ({
       selectEmpty: {
         marginTop: theme.spacing(2),
       },
+      flex: {
+          display: "flex",
+      },
+      title: {
+        padding: "2rem",
+        textAlign: "center",
+      }
 }))
 
 
 const Home = () => {
 
     const classes = useStyles();
+    var date = new Date();
+    const lastDay = new Date(date.getFullYear(), date.getMonth() + 1, 0);
 
     return (
         <Container maxWidth="md">
-            <Grid container className={classes.root} justify="center">
+            <Grid container className={classes.root} justify="center" > 
             <Grid item xs={12}>
+                <Typography variant='h4' className={classes.title}> Sistema de gestión de reservas de escritorios</Typography>
+            </Grid>
+            <Grid item xs={12} justify="center" className={classes.flex}>
                 <MuiPickersUtilsProvider utils={DateFnsUtils}>
                     <KeyboardDatePicker
                         disableToolbar
@@ -35,6 +48,7 @@ const Home = () => {
                             'aria-label': 'change date',
                         }}
                         disablePast={true}
+                        maxDate={lastDay}
                     />
                 </MuiPickersUtilsProvider>
             </Grid>
